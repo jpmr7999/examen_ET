f06b3f705fad8a8d242115907af2eb9c9dd69098
juan <juanpablomora36@gmail.com>
2024-11-05 10:20:24 -0300

    nuevoqr

--- src/app/pages/escanearqr/escanearqr.module.ts
+++ src/app/pages/escanearqr/escanearqr.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { QRCodeModule } from 'angularx-qrcode';
+import { AppComponent } from 'src/app/app.component';
 
-import { EscanearqrPageRoutingModule } from './escanearqr-routing.module';
 
-import { EscanearqrPage } from './escanearqr.page';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    EscanearqrPageRoutingModule
+    QRCodeModule,
   ],
-  declarations: [EscanearqrPage]
+  providers: [],
+bootstrap: [AppComponent]
 })
 export class EscanearqrPageModule {}
